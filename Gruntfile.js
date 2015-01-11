bda370a62d9ecec0f8418d981f3fcbf904beece2
Abed Halawi <halawi.abed@gmail.com>
2015-01-11 14:06:30 +0200

    re-order gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,79 +1,75 @@
-/*global module:false*/
+/* global module:false */
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    // Metadata.
-    meta: {
-      version: '0.1.0'
-    },
-    banner: '/*! MrUploader - v<%= meta.version %> - ' +
-      '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
-      '* http://github.com/vinelab/mr-uploader\n' +
-      '* Copyright (c) <%= grunt.template.today("yyyy") %> ' +
-      'Vinelab; Licensed MIT */\n',
+    // Project configuration.
+    grunt.initConfig({
+        // Metadata.
+        meta: {
+            version: '0.1.0'
+        },
+        banner: '/*! MrUploader - v<%= meta.version %> - ' +
+            '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+            '* http://github.com/vinelab/mr-uploader\n' +
+            '* Copyright (c) <%= grunt.template.today("yyyy") %> ' +
+            'Vinelab; Licensed MIT */\n'
+    });
+
     // Task configuration.
-    concat: {
-      options: {
-        banner: '<%= banner %>',
-        stripBanners: true
-      },
-      dist: {
-        src: ['vendors/jquery.Jcrop.min.js', 'dist/mr-uploader.js'],
-        dest: 'dist/js/mr-uploader.all.js'
-      },
-      css: {
-        src: ['src/style.css', 'vendors/jquery.Jcrop.min.css'],
-        dest: 'dist/css/mr-uploader.css'
-      }
-    },
-    uglify: {
-      options: {
-        banner: '<%= banner %>',
-        mangle: false
-      },
-      dist: {
-        src: '<%= concat.dist.dest %>',
-        dest: 'dist/js/mr-uploader.all.min.js'
-      }
-    },
-    coffee: {
-        compile: {
-          options: {
-            bare: true
-          },
-          files: {
-            'dist/js/mr-uploader.js': 'src/uploader.coffee'
-          }
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.config('concat', {
+        options: {
+            banner: '<%= banner %>',
+            stripBanners: true
+        },
+        dist: {
+            src: ['vendors/jquery.Jcrop.min.js', 'dist/mr-uploader.js'],
+            dest: 'dist/js/mr-uploader.all.js'
+        },
+        css: {
+            src: ['src/style.css', 'vendors/jquery.Jcrop.min.css'],
+            dest: 'dist/css/mr-uploader.css'
         }
-    },
-    cssmin: {
-      style: {
-        files: [{
-          expand: true,
-          cwd: 'dist/css',
-          src: ['*.css'],
-          dest: 'dist/css',
-          ext: '.min.css'
-        }]
-      }
-    },
-    watch: {
-      coffee: {
-        files: ['src/**/*'],
-        tasks: ['coffee', 'concat', 'uglify', 'cssmin']
-      }
-    }
-  });
+    });
 
-  // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-coffee');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.config('uglify', {
+        options: {
+            banner: '<%= banner %>',
+            mangle: false
+        },
+        all: {
+            src: '<%= concat.dist.dest %>',
+            dest: 'dist/js/mr-uploader.all.min.js'
+        }
+    });
 
-  // Default task.
-  // grunt.registerTask('default', ['jshint', 'qunit', 'concat', 'uglify']);
+    grunt.loadNpmTasks('grunt-contrib-coffee');
+    grunt.config('coffee', {
+        compile: {
+            options: {bare: true},
+            files: {'dist/js/mr-uploader.js': 'src/uploader.coffee'}
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.config('cssmin', {
+        style: {
+            files: [{
+                expand: true,
+                cwd: 'dist/css',
+                src: ['*.css'],
+                dest: 'dist/css',
+                ext: '.min.css'
+            }]
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.config('watch', {
+        coffee: {
+            files: ['src/**/*'],
+            tasks: ['coffee', 'concat', 'uglify', 'cssmin']
+        }
+    });
 
 };
